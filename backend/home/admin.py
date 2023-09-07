64c77a9d8df167df03890021a0cc03b378212c3d
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-07 20:23:20 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ShoppingCart
+admin.site.register(ShoppingCart)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class ShoppingCart(models.Model):
+    'Generated Model'
+    test = models.BigIntegerField()
+    user = models.OneToOneField("users.User",on_delete=models.CASCADE,null=True,blank=True,related_name="shoppingcart_user",)
